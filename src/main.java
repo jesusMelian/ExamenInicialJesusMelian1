aff4ab68b0fee50cb97d978fffe010b06f87fe73
jesusMelian <usr2685@salesianos-lacuesta.net>
2021-09-23 13:58:53 +0100

    Feature: añadido método obtenerPersonasAleatorias()
    Ese método mete en un ArrayList de String 10 personas aleatorias

--- src/main.java
+++ src/main.java
@@ -3,24 +3,32 @@ import java.util.ArrayList;
 
 public class main {
     public static void main(String[] args) {
-        obtenerPersonas();
+        //obtenerPersonas();
+        //System.out.println(contarPersonas(obtenerPersonas()));
+        //System.out.println(obtenerNumAleatorio(0, contarPersonas(obtenerPersonas())));
+        System.out.println("TAMAÑO: " +obtenerPersonasAleatorias().size());
+        for (int i = 0; i < 10; i++) {
+            System.out.println(obtenerPersonasAleatorias().get(i));
+        }
+
     }
 
     private static ArrayList<persona> obtenerPersonas(){
         ArrayList<persona> arrPersonas=new ArrayList<>();
         String nombre;
         File file = new File("listadodenombresaleatorios.csv");
+        //System.out.println("es: " +file.getAbsolutePath());
         try {
             BufferedReader bfR=new BufferedReader(new FileReader(file));
             //Para saltarme la primera linea que es la cabecera
-            //bfR.readLine();
+            bfR.readLine();
             //Bucle para recorrer las lineas del csv
             while ((nombre=bfR.readLine()) != null){
                 //Creo una nueva persona
                 persona person=new persona(nombre);
                 //añado la persona al arrayList de personas
                 arrPersonas.add(person);
-                System.out.println(String.valueOf(person.nombre));
+                //System.out.println(String.valueOf(person.nombre));
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -36,11 +44,26 @@ public class main {
     }
 
     private static double obtenerNumAleatorio(int minValue, int maxValue){
-        double randomNum = Math.random() * ( maxValue - minValue);
+        double randomNum = Math.random() * (maxValue - minValue);
         System.out.println("Random Number: "+randomNum);
         return  randomNum;
     }
-    public static ArrayList<String> obtenerPersonasAleatorias
+
+    public static ArrayList<String> obtenerPersonasAleatorias(){
+        double numAleatorio=0;
+        ArrayList<String> arr10Personas=new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            //obtengo un número aleatorio dentro del rango.
+            numAleatorio=obtenerNumAleatorio(0, contarPersonas(obtenerPersonas()));
+
+            System.out.println(i+"- "+ obtenerPersonas().get((int) numAleatorio).nombre);
+            arr10Personas.add(obtenerPersonas().get((int) numAleatorio).nombre);
+        }
+        return  arr10Personas;
+    }
+
+    public static aniadirNotasAAlumno
 }
 
 
